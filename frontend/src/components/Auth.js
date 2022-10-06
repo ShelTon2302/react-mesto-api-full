0e06813371ee1fa997a57aade54c59b73d34f74d
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-10-06 17:56:58 +0300

    PR15 subdomen

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://auth.nomoreparties.co';
+const BASE_URL = 'https://api.project-mesto.nomoredomains.icu';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
