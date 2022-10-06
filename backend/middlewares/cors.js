7a77e1388e902cb60707b553cfefe63c894bc946
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-10-06 19:31:24 +0300

    PR15 login from front

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,19 +2,16 @@ const { allowedCors } = require('../const/const');
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE'; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE';
-
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
 
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
