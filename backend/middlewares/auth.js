4dc029e583e18a11ccbb6dbbfe1c38d25b629c35
Шеланов Антон Алексеевич <BT144@bunker-trade.local>
2022-10-12 19:44:47 +0300

    PR15 after review: another jwt_secret for dev

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,8 @@ const AuthError = require('../errors/auth-error');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
+  const { NODE_ENV, JWT_SECRET } = process.env;
+
   if (!token) {
     next(new AuthError('Необходима авторизация'));
     return;
@@ -11,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : '59c55acd63cac9349e3b2c538e86de0f');
   } catch (err) {
     next(new AuthError('Необходима авторизация'));
     return;
